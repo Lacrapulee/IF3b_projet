4f40f0fb7a66470f4d3f189643e239e0d9089375
Gauthier Noel <gautiernoel4@gmail.com>
2024-06-21 14:54:11 +0200

    code presque final

--- Arduino/ESP32/ESP32/ESP32.ino
+++ Arduino/ESP32/ESP32/ESP32.ino
@@ -1,72 +1,49 @@
-// Bibliotheque :
+// Bibliothèques
 #include <Wire.h>
 #include <cstring>
-//capteurs 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <DHT.h>
 #include "DFRobot_CCS811.h"
-//actionneurs
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-//Mqtt
-#include <WiFi.h>
-#include <PubSubClient.h>
-
-//led 
+// Définition des pins
 #define LED_PIN 4
 #define LED_COUNT 8
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
-
-//bouton 
-const int buttonPin = 27;
-int buttonState = 0;
-
-//ventilateur 
-int ventilateur = 0; 
-
-// WiFi and MQTT Settings
-const char* ssid = "Gauthier";
-const char* password = "y3rfajkj";
-const char* mqtt_server = "mqtt.ci-ciad.utbm.fr";
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-
-// DHT11
-#define PIN_DHT11 22 
+#define PIN_DHT11 22
 #define DHTTYPE DHT11
-DHT dht(PIN_DHT11, DHTTYPE);
-
-//fan
-#define Fan 21    //define driver pins
-
-// BMP280 Sensor
-Adafruit_BMP280 bmp;
+#define Fan 21
 #define SDA_PIN 33
 #define SCL_PIN 32
-
-// OLED Display
 #define OLED_RESET 4
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
+#define buttonPin 27
+#define buzzerPin 14
 
-// Air Quality Sensor
+// Initialisation des objets
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+DHT dht(PIN_DHT11, DHTTYPE);
+Adafruit_BMP280 bmp;
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
 DFRobot_CCS811 CCS811;
 
-//Define buzzerPin
-int buzzerPin = 14; 
+// Variables globales
+int ventilateur = 0;
+int ventilateur_auto = 0;
+int buttonState = 0;
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+const char* ssid = "Gauthier";
+const char* password = "y3rfajkj";
+const char* mqtt_server = "mqtt.ci-ciad.utbm.fr";
 
-// Function to connect to WiFi
+// Fonction de connexion WiFi
 void setup_wifi() {
   delay(10);
   Serial.println();
@@ -83,7 +60,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-// MQTT callback function
+// Fonction de rappel MQTT
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
@@ -94,27 +71,22 @@ void callback(char* topic, byte* message, unsigned int length) {
     messageTemp += (char)message[i];
   }
   Serial.println();
-  if (strcmp(topic, "esp32/ventilateur") == 0){
-    if (messageTemp == "ON") {
-      Serial.println("boutton appuyé");
-      if(ventilateur == 4){
-        ventilateur = 0; 
-      }else{
-        ventilateur += 1;
-      }
+  if (strcmp(topic, "esp32/ventilateur") == 0) {
+    if (messageTemp == "ON" && ventilateur_auto != 256) {
+      Serial.println("bouton appuyé");
+      ventilateur = (ventilateur == 4) ? 0 : ventilateur + 1;
     } 
-  
+  }
 }
 
-// Function to reconnect to MQTT
+// Fonction de reconnexion MQTT
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("espclient")) {
       Serial.println("connected");
       client.subscribe("esp32/projet_qualitee_air");
-      clientNOEL.subscribe("esp32/ventilateur");
-
+      client.subscribe("esp32/ventilateur");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -125,44 +97,26 @@ void reconnect() {
 }
 
 void setup() {
-  // Serial communication
+  // Initialisation série
   Serial.begin(115200);
 
-  
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-    ^m^!// clock_prescale_set(clock_div_1);
-  #endif
-  // END of Trinket-specific code.
-
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
-
-  //ventilateur 
-  pinMode(Fan,OUTPUT);
-  
-  //bouton
+  // Initialisation des actionneurs et capteurs
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
+  pinMode(Fan, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
-
   Wire.begin(SDA_PIN, SCL_PIN);
+  pinMode(buzzerPin, OUTPUT);
 
-  pinMode(buzzerPin, OUTPUT); //Set buzzerPin as output
-
-  // BMP280 sensor
+  // Initialisation des capteurs
   if (!bmp.begin(0x76)) {
     Serial.println(F("Could not find BMP280 sensor. Check wiring."));
     while (1);
   }
 
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
-  Adafruit_BMP280::SAMPLING_X2,
-  Adafruit_BMP280::SAMPLING_X16,
-  Adafruit_BMP280::FILTER_X16,
-  Adafruit_BMP280::STANDBY_MS_500);
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, Adafruit_BMP280::SAMPLING_X2, Adafruit_BMP280::SAMPLING_X16, Adafruit_BMP280::FILTER_X16, Adafruit_BMP280::STANDBY_MS_500);
 
-  // OLED display
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("Could not find OLED display. Check wiring."));
     while (1);
@@ -171,35 +125,32 @@ void setup() {
   delay(2000);
   display.clearDisplay();
 
-  // DHT 11 sensor 
-  pinMode(PIN_DHT11, INPUT);
   dht.begin();
 
-  // CCS811 sensor
   while (CCS811.begin() != 0) {
     Serial.println("Failed to init CCS811 chip. Please check the connection.");
     delay(1000);
   }
 
-  // WiFi and MQTT
+  // Initialisation WiFi et MQTT
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
-  // Read DHT sensor data  
-  float humidity = dht.readHumidity();  
-  if (isnan(humidity) ) {
-    Serial.println("Echec reception, d'un ou des capteurs");
+  // Lecture des capteurs
+  float humidity = dht.readHumidity();
+  float temperature = bmp.readTemperature();
+  float pressure = bmp.readPressure() / 100.0F; // conversion en hPa
+  float altitude = bmp.readAltitude(1013.25); // pression au niveau de la mer en hPa
+
+  if (isnan(humidity) || isnan(temperature) || isnan(pressure) || isnan(altitude)) {
+    Serial.println("Echec de reception d'un ou des capteurs");
     return;
   }
-  // Read BMP280 sensor data
-  float temperature = bmp.readTemperature();
-  float pressure = bmp.readPressure() / 100.0F; // convert to hPa
-  float altitude = bmp.readAltitude(1013.25); // sea level pressure in hPa
 
-  // Display data on OLED
+  // Affichage sur l'écran OLED
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
@@ -214,7 +165,6 @@ void loop() {
   display.print(altitude);
   display.println(" m");
 
-
   if (CCS811.checkDataReady() == true) {
     display.print("CO2: ");
     display.print(CCS811.getCO2PPM());
@@ -222,60 +172,68 @@ void loop() {
     display.print(CCS811.getTVOCPPB());
     display.println("ppb");
 
-    if (CCS811.getCO2PPM()> 600){
-      
-      analogWrite(buzzerPin, 200); //Setting pin to high
-      delay(50); //Delaying
-      analogWrite(buzzerPin ,0); //Setting pin to LOW 
+    if (CCS811.getCO2PPM() > 1000) {
+      analogWrite(buzzerPin, 200); // Activation du buzzer
+      delay(50);
+      analogWrite(buzzerPin, 0); // Désactivation du buzzer
       Serial.print("buzzer");
+      ventilateur_auto = 256;
+    }
+    else{
+      ventilateur_auto = 0; 
     }
 
-    Serial.print("Humidite : ");
+    Serial.print("Humidité : ");
     Serial.println(humidity);
     Serial.print("CO2: ");
     Serial.print(CCS811.getCO2PPM());
     Serial.print("ppm, TVOC: ");
     Serial.print(CCS811.getTVOCPPB());
     Serial.println("ppb");
-
-
   }
+
   display.display();
 
   CCS811.writeBaseLine(0x447B);
 
-  //relatif au ventilateur 
-  if (ventilateur == 0){
-      strip.clear(); 
+  // Gestion du ventilateur et des LED
+  switch (ventilateur) {
+    case 0:
+      strip.clear();
       strip.show();
-    }else if (ventilateur ==  1){
-      strip.setPixelColor(0, strip.Color(50, 50, 50));  
-      strip.setPixelColor(1, strip.Color(50, 50, 50));  
+      break;
+    case 1:
+      strip.setPixelColor(0, strip.Color(50, 50, 50));
+      strip.setPixelColor(1, strip.Color(50, 50, 50));
       strip.show();
-    }else if (ventilateur == 2){ 
-      strip.setPixelColor(2, strip.Color(50, 50, 50));  
-      strip.setPixelColor(3, strip.Color(50, 50, 50));  
+      break;
+    case 2:
+      strip.setPixelColor(2, strip.Color(50, 50, 50));
+      strip.setPixelColor(3, strip.Color(50, 50, 50));
       strip.show();
-    }else if (ventilateur == 3){ 
-      strip.setPixelColor(4, strip.Color(50, 50, 50));  
-      strip.setPixelColor(5, strip.Color(50, 50, 50));  
+      break;
+    case 3:
+      strip.setPixelColor(4, strip.Color(50, 50, 50));
+      strip.setPixelColor(5, strip.Color(50, 50, 50));
       strip.show();
-    }else if (ventilateur == 4){ 
-      strip.setPixelColor(6, strip.Color(50, 50, 50));  
-      strip.setPixelColor(7, strip.Color(50, 50, 50));  
+      break;
+    case 4:
+      strip.setPixelColor(6, strip.Color(50, 50, 50));
+      strip.setPixelColor(7, strip.Color(50, 50, 50));
       strip.show();
-    }
-  analogWrite(Fan, ventilateur*64);
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW) {
-    if(ventilateur == 4){
-      ventilateur = 0; 
-    }else{
-      ventilateur += 1;
-    }
+      break;
   }
 
-  // MQTT handling
+  analogWrite(Fan, ventilateur * 64);
+  buttonState = digitalRead(buttonPin);
+  if (ventilateur_auto == 256){
+    analogWrite(Fan, ventilateur_auto);
+  }
+  if (buttonState == LOW && ventilateur_auto != 256) {
+    ventilateur = (ventilateur == 4) ? 0 : ventilateur + 1;
+  }
+
+  // Gestion du MQTT
   if (!client.connected()) {
     reconnect();
   }
@@ -300,9 +258,9 @@ void loop() {
 
     char result[55]; // Taille de la chaîne de résultat (5 valeurs * 8 caractères chacune + 4 tirets + 1 pour le caractère de fin de chaîne)
     snprintf(result, sizeof(result), "%.2f-%s-%.2f-%.2f-%s-%s", temperature, presString, altitude, humidity, CO2String, TVOCPPBString);
+
+    // Publication des données sur le topic
     client.publish("esp32/projet_qualitee_air", result);
   }
-
-  // Delay before next loop iteration
-  delay(5000);
+  delay(200);
 }
