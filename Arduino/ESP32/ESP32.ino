1f6013a286d0d9aded56bb616fe76aebba4ac5ff
Gauthier NOEL <115105753+Lacrapulee@users.noreply.github.com>
2024-06-24 13:22:47 +0200

    fin

--- Arduino/ESP32/ESP32.ino
+++ Arduino/ESP32/ESP32.ino
@@ -1,72 +1,54 @@
-// Bibliotheque :
 #include <Wire.h>
 #include <cstring>
-//capteurs 
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BMP280.h>
 #include <DHT.h>
 #include "DFRobot_CCS811.h"
-//actionneurs
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
- #include <avr/power.h> // Required for 16 MHz Adafruit Trinket
-#endif
 
-//Mqtt
-#include <WiFi.h>
-#include <PubSubClient.h>
-
-//led 
 #define LED_PIN 4
 #define LED_COUNT 8
-// Declare our NeoPixel strip object:
-Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
-
-//bouton 
-const int buttonPin = 27;
-int buttonState = 0;
-
-//ventilateur 
-int ventilateur = 0; 
-
-// WiFi and MQTT Settings
-const char* ssid = "Gauthier";
-const char* password = "y3rfajkj";
-const char* mqtt_server = "mqtt.ci-ciad.utbm.fr";
-WiFiClient espClient;
-PubSubClient client(espClient);
-long lastMsg = 0;
-
-// DHT11
-#define PIN_DHT11 22 
+#define PIN_DHT11 22
 #define DHTTYPE DHT11
-DHT dht(PIN_DHT11, DHTTYPE);
-
-//fan
-#define Fan 21    //define driver pins
-
-// BMP280 Sensor
-Adafruit_BMP280 bmp;
+#define Fan 21
 #define SDA_PIN 33
 #define SCL_PIN 32
-
-// OLED Display
 #define OLED_RESET 4
 #define OLED_WIDTH 128
 #define OLED_HEIGHT 64
-Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
+#define buttonPin 27
+#define buzzerPin 34
 
-// Air Quality Sensor
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_RGB + NEO_KHZ800);
+DHT dht(PIN_DHT11, DHTTYPE);
+Adafruit_BMP280 bmp;
+Adafruit_SSD1306 display(OLED_WIDTH, OLED_HEIGHT, &Wire, OLED_RESET);
 DFRobot_CCS811 CCS811;
 
-//Define buzzerPin
-int buzzerPin = 14; 
+const int MAX = 120;
+int tvocArray[MAX];
+int co2Array[MAX];
+int tempArray[MAX]; 
+int humArray[MAX];
+int ventilateur = 0;
+int ecran = 0;
+int buttonState = HIGH;
+int lastButtonState = HIGH;
+unsigned long lastDebounceTime = 0;
+const unsigned long debounceDelay = 25;
 
-// Function to connect to WiFi
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+const char* ssid = "Gauthier";
+const char* password = "y3rfajkj";
+const char* mqtt_server = "mqtt.ci-ciad.utbm.fr";
+
+// Fonction de connexion WiFi
 void setup_wifi() {
   delay(10);
   Serial.println();
@@ -83,7 +65,7 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-// MQTT callback function
+// Fonction de rappel MQTT
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.print("Message arrived on topic: ");
   Serial.print(topic);
@@ -94,27 +76,28 @@ void callback(char* topic, byte* message, unsigned int length) {
     messageTemp += (char)message[i];
   }
   Serial.println();
-  if (strcmp(topic, "esp32/ventilateur") == 0){
+  if (strcmp(topic, "esp32/projet_qualitee_air/Z/ecran") == 0) {
+    ecran = atoi(messageTemp.c_str());
+    Serial.println("ecran");  
+    
+  }
+  if (strcmp(topic, "esp32/projet_qualitee_air/Z/ventilateur") == 0) {
     if (messageTemp == "ON") {
-      Serial.println("boutton appuyé");
-      if(ventilateur == 4){
-        ventilateur = 0; 
-      }else{
-        ventilateur += 1;
-      }
+      Serial.println("bouton appuyé");
+      ventilateur = (ventilateur == 4) ? 0 : ventilateur + 1;
     } 
-  
+  }
 }
 
-// Function to reconnect to MQTT
+// Fonction de reconnexion MQTT
 void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     if (client.connect("espclient")) {
       Serial.println("connected");
-      client.subscribe("esp32/projet_qualitee_air");
-      clientNOEL.subscribe("esp32/ventilateur");
-
+      client.subscribe("esp32/projet_qualitee_air/Z");
+      client.subscribe("esp32/projet_qualitee_air/Z/ventilateur");
+      client.subscribe("esp32/projet_qualitee_air/Z/ecran");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -125,44 +108,26 @@ void reconnect() {
 }
 
 void setup() {
-  // Serial communication
+  // Initialisation série
   Serial.begin(115200);
 
-  
-  // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
-  // Any other board, you can remove this part (but no harm leaving it):
-  #if defined(__AVR_ATtiny85__) && (F_CPU == 16000000)
-    ^m^!// clock_prescale_set(clock_div_1);
-  #endif
-  // END of Trinket-specific code.
-
-  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
-  strip.show();            // Turn OFF all pixels ASAP
-  strip.setBrightness(50); // Set BRIGHTNESS to about 1/5 (max = 255)
-
-  //ventilateur 
-  pinMode(Fan,OUTPUT);
-  
-  //bouton
+  // Initialisation des actionneurs et capteurs
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
+  pinMode(Fan, OUTPUT);
   pinMode(buttonPin, INPUT_PULLUP);
-
   Wire.begin(SDA_PIN, SCL_PIN);
+  pinMode(buzzerPin, OUTPUT);
 
-  pinMode(buzzerPin, OUTPUT); //Set buzzerPin as output
-
-  // BMP280 sensor
+  // Initialisation des capteurs
   if (!bmp.begin(0x76)) {
     Serial.println(F("Could not find BMP280 sensor. Check wiring."));
     while (1);
   }
 
-  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,
-  Adafruit_BMP280::SAMPLING_X2,
-  Adafruit_BMP280::SAMPLING_X16,
-  Adafruit_BMP280::FILTER_X16,
-  Adafruit_BMP280::STANDBY_MS_500);
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL, Adafruit_BMP280::SAMPLING_X2, Adafruit_BMP280::SAMPLING_X16, Adafruit_BMP280::FILTER_X16, Adafruit_BMP280::STANDBY_MS_500);
 
-  // OLED display
   if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
     Serial.println(F("Could not find OLED display. Check wiring."));
     while (1);
@@ -171,119 +136,143 @@ void setup() {
   delay(2000);
   display.clearDisplay();
 
-  // DHT 11 sensor 
-  pinMode(PIN_DHT11, INPUT);
   dht.begin();
 
-  // CCS811 sensor
   while (CCS811.begin() != 0) {
     Serial.println("Failed to init CCS811 chip. Please check the connection.");
     delay(1000);
   }
-
-  // WiFi and MQTT
+  // Initialisation WiFi et MQTT
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
 }
 
 void loop() {
-  // Read DHT sensor data  
-  float humidity = dht.readHumidity();  
-  if (isnan(humidity) ) {
-    Serial.println("Echec reception, d'un ou des capteurs");
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  float pressure = bmp.readPressure() / 100.0F;
+  float altitude = bmp.readAltitude(1013.25);
+  int CO2 = 0; 
+  int TVOC = 0;
+  if (CCS811.checkDataReady() == true) {
+    CO2 = CCS811.getCO2PPM(); 
+    TVOC = CCS811.getTVOCPPB(); 
+  }
+  
+  storeTemp();
+
+  if (isnan(humidity) || isnan(temperature) || isnan(pressure) || isnan(altitude)) {
+    Serial.println("Echec de reception d'un ou des capteurs");
     return;
   }
-  // Read BMP280 sensor data
-  float temperature = bmp.readTemperature();
-  float pressure = bmp.readPressure() / 100.0F; // convert to hPa
-  float altitude = bmp.readAltitude(1013.25); // sea level pressure in hPa
 
-  // Display data on OLED
+  // Affichage sur l'écran OLED 
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0, 0);
-  display.print(F("Temperature: "));
-  display.print(temperature);
-  display.println(" *C");
-  display.print(F("Pressure: "));
-  display.print(pressure);
-  display.println(" hPa");
-  display.print(F("Altitude: "));
-  display.print(altitude);
-  display.println(" m");
-
-
-  if (CCS811.checkDataReady() == true) {
-    display.print("CO2: ");
-    display.print(CCS811.getCO2PPM());
-    display.print("ppm, TVOC: ");
-    display.print(CCS811.getTVOCPPB());
-    display.println("ppb");
-
-    if (CCS811.getCO2PPM()> 600){
-      
-      analogWrite(buzzerPin, 200); //Setting pin to high
-      delay(50); //Delaying
-      analogWrite(buzzerPin ,0); //Setting pin to LOW 
-      Serial.print("buzzer");
-    }
-
-    Serial.print("Humidite : ");
-    Serial.println(humidity);
-    Serial.print("CO2: ");
-    Serial.print(CCS811.getCO2PPM());
-    Serial.print("ppm, TVOC: ");
-    Serial.print(CCS811.getTVOCPPB());
-    Serial.println("ppb");
-
-
+  display.setCursor(0, 0); 
+  switch (ecran) {
+    case 0: 
+      display.print("Temperature : ");
+      display.print(temperature);
+      display.println("*C");
+      drawTempGraph();
+      break; 
+    case 1: 
+      display.print("  Pression: ");
+      display.print(pressure);
+      display.println("hPa");
+      break; 
+    case 2: 
+      display.print("  Altitude: ");
+      display.print(altitude);
+      display.println("m");
+      break; 
+    case 3: 
+      display.print("  Humiditee : ");
+      display.print(humidity);
+      display.println("%");
+      drawHumGraph();
+      break;
+    case 4: 
+      display.print("  CO2: ");
+      display.print(CO2);
+      display.println("ppm");
+      drawCo2Graph();
+      break; 
+    case 5: 
+      display.print("    TVOC: ");
+      display.print(TVOC);
+      display.println("ppb");
+      drawTvocGraph();
+      break; 
   }
   display.display();
-
   CCS811.writeBaseLine(0x447B);
 
-  //relatif au ventilateur 
-  if (ventilateur == 0){
-      strip.clear(); 
-      strip.show();
-    }else if (ventilateur ==  1){
-      strip.setPixelColor(0, strip.Color(50, 50, 50));  
-      strip.setPixelColor(1, strip.Color(50, 50, 50));  
-      strip.show();
-    }else if (ventilateur == 2){ 
-      strip.setPixelColor(2, strip.Color(50, 50, 50));  
-      strip.setPixelColor(3, strip.Color(50, 50, 50));  
-      strip.show();
-    }else if (ventilateur == 3){ 
-      strip.setPixelColor(4, strip.Color(50, 50, 50));  
-      strip.setPixelColor(5, strip.Color(50, 50, 50));  
-      strip.show();
-    }else if (ventilateur == 4){ 
-      strip.setPixelColor(6, strip.Color(50, 50, 50));  
-      strip.setPixelColor(7, strip.Color(50, 50, 50));  
-      strip.show();
-    }
-  analogWrite(Fan, ventilateur*64);
-  buttonState = digitalRead(buttonPin);
-  if (buttonState == LOW) {
-    if(ventilateur == 4){
-      ventilateur = 0; 
-    }else{
-      ventilateur += 1;
-    }
+
+
+  if (CO2 > 1000) {
+    digitalWrite(buzzerPin, HIGH);
+    analogWrite(Fan, 256);
+  } else {
+    digitalWrite(buzzerPin, LOW);
+    analogWrite(Fan, ventilateur * 64);
   }
 
-  // MQTT handling
+  
+  int reading = digitalRead(buttonPin);
+  if (reading != lastButtonState) {
+    lastDebounceTime = millis();
+  }
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    if (reading != buttonState) {
+      buttonState = reading;
+      if (buttonState == LOW) {
+        ventilateur = (ventilateur == 4) ? 0 : ventilateur + 1;
+      }
+    }
+  }
+  lastButtonState = reading;
+
+  switch (ventilateur) {
+    case 0:
+      strip.clear();
+      strip.show();
+      break;
+    case 1:
+      strip.setPixelColor(7, strip.Color(50, 50, 50));
+      strip.setPixelColor(6, strip.Color(50, 50, 50));
+      strip.show();
+      break;
+    case 2:
+      strip.setPixelColor(5, strip.Color(50, 50, 50));
+      strip.setPixelColor(4, strip.Color(50, 50, 50));
+      strip.show();
+      break;
+    case 3:
+      strip.setPixelColor(3, strip.Color(50, 50, 50));
+      strip.setPixelColor(2, strip.Color(50, 50, 50));
+      strip.show();
+      break;
+    case 4:
+      strip.setPixelColor(1, strip.Color(50, 50, 50));
+      strip.setPixelColor(0, strip.Color(50, 50, 50));
+      strip.show();
+      break;
+  }
+
+
+
   if (!client.connected()) {
     reconnect();
   }
   client.loop();
+
   long now = millis();
-  if (now - lastMsg > 2000) {
+  if (now - lastMsg > 60000) {
     lastMsg = now;
-    // Convertir les valeurs float en chaînes de caractères
     char tempString[8];
     char presString[8];
     char altitudeString[8];
@@ -291,18 +280,114 @@ void loop() {
     char CO2String[8];
     char TVOCPPBString[8];
 
+
     dtostrf(pressure, 1, 2, presString);
     dtostrf(temperature, 1, 2, tempString);
     dtostrf(altitude, 1, 2, altitudeString);
     dtostrf(humidity, 1, 2, humidityString);
-    sprintf(CO2String, "%u", CCS811.getCO2PPM()); // Utilisation de %u pour un uint16_t
-    sprintf(TVOCPPBString, "%u", CCS811.getTVOCPPB()); // Utilisation de %u pour un uint16_t
-
+    sprintf(CO2String, "%u", CO2);
+    sprintf(TVOCPPBString, "%u", TVOC);
     char result[55]; // Taille de la chaîne de résultat (5 valeurs * 8 caractères chacune + 4 tirets + 1 pour le caractère de fin de chaîne)
     snprintf(result, sizeof(result), "%.2f-%s-%.2f-%.2f-%s-%s", temperature, presString, altitude, humidity, CO2String, TVOCPPBString);
-    client.publish("esp32/projet_qualitee_air", result);
+    client.publish("esp32/projet_qualitee_air/Z", result);
   }
-
-  // Delay before next loop iteration
-  delay(5000);
+  delay(200);
 }
+void storeTemp()
+{
+  int temp = dht.readTemperature();
+  int hum = dht.readHumidity(); 
+  int co2 = CCS811.getCO2PPM(); 
+  int tvoc = CCS811.getTVOCPPB();
+  static int i = 0;
+  if ( isnan( ( int ) temp ) )
+    if ( i < MAX )
+    {
+      tempArray[ i ] = 0;
+      humArray[ i ] = 0;
+      i++;
+    }
+    else
+    {
+      for ( int j = 0; j < MAX - 1; j++ )
+      {
+        tempArray[ j ] = tempArray[ j + 1 ];
+        tempArray[ MAX - 1 ] = 0;
+        co2Array[j] = co2Array[j + 1];
+        co2Array[ MAX - 1 ] = 0;
+        tvocArray[j] = tvocArray[j + 1];
+        tvocArray[ MAX - 1 ] = 0;
+        humArray[ j ] = humArray[ j + 1 ];
+        humArray[ MAX - 1 ] = 0; 
+      }
+    }
+  else
+  {
+    if ( i < MAX )
+    {
+      tempArray[ i ] = temp;
+      co2Array[ i ] = co2;
+      humArray[ i ] = hum;
+      tvocArray[ i ] = tvoc;
+      i++;
+    }
+    else
+    {
+      for ( int j = 0; j < MAX - 1; j++ )
+      {
+        tempArray[ j ] = tempArray[ j + 1 ];
+        tempArray[ MAX - 1 ] = temp;
+        co2Array[j] = co2Array[j + 1];
+        co2Array[ MAX - 1 ] = co2;
+        tvocArray[j] = tvocArray[j + 1];
+        tvocArray[ MAX - 1 ] = tvoc;
+        humArray[ j ] = humArray[ j + 1 ];
+        humArray[ MAX - 1 ] = hum; 
+      }
+    }
+  }
+}
+
+void drawGraph()
+{
+  display.drawPixel( 6, 13, WHITE ); 
+  display.drawPixel( 6, 23, WHITE ); 
+  display.drawPixel( 6, 33, WHITE ); 
+  display.drawPixel( 6, 43, WHITE ); 
+  display.drawPixel( 6, 53, WHITE ); 
+  display.drawPixel( 27, 62, WHITE ); 
+  display.drawPixel( 47, 62, WHITE ); 
+  display.drawPixel( 67, 62, WHITE ); 
+  display.drawPixel( 87, 62, WHITE ); 
+  display.drawPixel( 107, 62, WHITE ); 
+  display.drawFastVLine( 7, 15, 100, WHITE );
+  display.drawFastHLine( 7, 63, 120, WHITE );
+}
+void drawTempGraph()
+{
+  drawGraph();
+  for (int i = 0; i < MAX; i++ )
+    display.drawFastHLine( 135 - MAX * 2 + i * 2, 64 - tempArray[ i ], 2, WHITE  ); 
+}
+void drawHumGraph()
+{
+  drawGraph();
+  for (int i = 0; i < MAX; i++ )
+    display.drawFastHLine( 135 - MAX + i * 2, 64 - humArray[ i ] /2, 2,  WHITE  ); 
+
+}
+void drawCo2Graph()
+{
+  drawGraph();
+  for (int i = 0; i < MAX; i++ )
+    display.drawFastHLine( 135 - MAX + i * 2, 64 - co2Array[ i ] /15 , 2,  WHITE  ); 
+
+}
+void drawTvocGraph()
+{
+  drawGraph();
+  for (int i = 0; i < MAX; i++ )
+    display.drawFastHLine( 135 - MAX + i * 2, 64 - tvocArray[ i ]*2, 2,  WHITE  ); 
+
+}
+
